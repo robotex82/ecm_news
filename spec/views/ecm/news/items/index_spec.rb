d7132f6072c9ac2dc668741949ada51146b26fc4
Roberto Vasquez Angel <robotex@robotex.de>
2012-08-28 17:11:51 +0200

    Fixed tests.

--- spec/views/ecm/news/items/index_spec.rb
+++ spec/views/ecm/news/items/index_spec.rb
@@ -6,13 +6,14 @@ describe "ecm/news/items/index" do
   end
     
   it "renders _item_preview partial for each news item" do
-    assign(:items, [stub_model(Ecm::News::Item), stub_model(Ecm::News::Item)])
+    items = FactoryGirl.create_list(:ecm_news_item, 3)
+    assign(:items, items)
     render
-    view.should render_template(:partial => "_item_preview", :count => 2)
+    view.should render_template(:partial => "_item_preview", :count => 3)
   end
   
   it "shows headings for all items" do
-    items = FactoryGirl.create_list(:ecm_news_item, 5)
+    items = FactoryGirl.create_list(:ecm_news_item, 3)
     assign(:items, items)
     render
     items.each do |i|
@@ -21,11 +22,11 @@ describe "ecm/news/items/index" do
   end
   
   it "shows links to all items" do
-    items = FactoryGirl.create_list(:ecm_news_item, 5)
+    items = FactoryGirl.create_list(:ecm_news_item, 3)
     assign(:items, items)
     render
     items.each do |i|
-      rendered.should have_link("more", :href => "/ecm_news_items/#{i.to_param}")
+      rendered.should have_link("continue reading", :href => "/ecm_news_items/#{i.to_param}")
     end
   end
 end
